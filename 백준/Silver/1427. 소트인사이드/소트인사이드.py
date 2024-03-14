9bdf1448c2dcc2795ddf0a765237d701839f3bd3
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-03-14 23:48:55 +0900

    [Silver V] Title: 소트인사이드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,6 +1,4 @@
 N = input()
-num_list=[]
-for i in N:
-    num_list.append(int(i))
-num_list.sort(reverse=True)
-print(str(num_list).replace(', ','').replace('[','').replace(']',''))
+sorted_digits = sorted(N, reverse=True)
+result = ''.join(sorted_digits)
+print(result)
