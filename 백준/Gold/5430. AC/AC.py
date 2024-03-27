ca8ba515d531592d75a9d053ae64696cdd5a7199
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-03-27 14:33:06 +0900

    [Gold V] Title: AC, Time: 696 ms, Memory: 41120 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -7,34 +7,23 @@ for _ in range(N):
     error=0
     cnt=0
     command=sys.stdin.readline()
-    #print('command=',command)
     n=int(sys.stdin.readline())
-    #number_list=[]
     number=sys.stdin.readline()
     number=number.replace('[','').replace(']','').replace('\n','')
-    # if command.count('D')>n:
-    #     print('error')
-
-    # if n!=1 and ',' not in number: #여기 수정 1 R 1일때 이상함
-    #     print('error')
-    #     continue
     if n!=0:
         try:
             number_list=[int(x) for x in number.split(',')]
         except:
             print('error')
             continue
-    #print('number=',number)
+    
         for j in number_list:
             if j!=',' and j!=']' and j!='[' and j!='\n':
                 deque.append(int(j))
     for i in command:
         if i=='R':
             cnt+=1
-            #print(cnt)
         elif i=='D':
-            #cnt=0
-            #print('cnt=',cnt)
             if len(deque)==0:
                 print('error')
                 error=1
@@ -45,7 +34,6 @@ for _ in range(N):
                 deque.pop()
 
     if error==0:
-        #print(deque)
         print('[',end='')
         while len(deque)>1:
             if cnt % 2 == 0:
@@ -55,6 +43,3 @@ for _ in range(N):
         if(len(deque)==1):
             print(deque.pop(),end='')
         print(']')
-
-
-
