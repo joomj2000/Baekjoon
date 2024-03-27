49dc2406953a0232671498c588f80bebe1530fc0
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-03-27 15:00:54 +0900

    [Gold V] Title: AC, Time: 404 ms, Memory: 41692 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,41 +1,42 @@
 import sys
 from collections import deque
-N = int(sys.stdin.readline())
-deque = deque([])
 
-def action(command):
-    cnt=0
-    for i in command:
-        if i == 'R':
+def action(p, deque): #언어 AC 실행
+    cnt = 0
+    for i in p:
+        if i == 'R': #뒤집기 일때 cnt증가
             cnt += 1
-        elif i == 'D':
-            if len(deque) == 0:
+        elif i == 'D': #버리기 일때
+            if len(deque) == 0: #deque가 비어있으면 에러 출력후 return
                 print('error')
                 return
-            if cnt % 2 == 0:
+            if cnt % 2 == 0: # 뒤집어져 있지 않은 상태일때 첫번째 수 버리기
                 deque.popleft()
-            else:
+            else: # 뒤집어져 있는 상태일때 마지막 수 버리기
                 deque.pop()
 
+    # 출력형식에 맞게 출력
     print('[', end='')
     while len(deque) > 1:
         if cnt % 2 == 0:
             print(deque.popleft(), end=',')
         else:
             print(deque.pop(), end=',')
-    if (len(deque) == 1):
+    if len(deque) == 1:
         print(deque.pop(), end='')
     print(']')
 
 
-for _ in range(N):
-    command = sys.stdin.readline()
-    n = int(sys.stdin.readline())
-    number = sys.stdin.readline()
-    number = number.replace('[', '').replace(']', '').replace('\n', '')
-    if n != 0:
-        number_list = [int(x) for x in number.split(',')]
-        for j in number_list:
-            deque.append(int(j))
+if __name__ == "__main__":
+    T = int(sys.stdin.readline()) #테스트 케이스 개수
+    deque = deque([]) #deque 선언
 
-    action(command)
+    for _ in range(T):
+        p = sys.stdin.readline().strip() #수행할 함수 입력
+        n = int(sys.stdin.readline())
+        number = sys.stdin.readline().strip()[1:-1] #대괄호로 둘러싸인 입력일때 값만 추출
+        if n != 0:
+            number_list = list(map(int, number.split(',')))#문자열로 입력받은 number을 ','로 split하여 number_list에 넣음
+            deque.extend(number_list) #number_list의 모슨요소를 덱에 추가
+
+        action(p, deque)#언어 AC 실행
