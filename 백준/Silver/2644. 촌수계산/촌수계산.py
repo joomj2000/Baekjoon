d970b6ce0cd1d77e3997347c34d53864ac70d715
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-03-31 21:15:21 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,30 +1,26 @@
 import sys
-from collections import deque
 
-def BFS(Graph, start):
-    visited = [-1] * (n + 1)
-    queue=deque([start])
-    visited[start]=0
-    while queue:
-        V=queue.popleft()
-        for i in Graph[V]:
-            if visited[i]==-1:
-                visited[i]=visited[V]+1
-                queue.append(i)
-    return visited
+
+def DFS(Graph, V, visited):
+    #visited[V] = 0
+    for i in Graph[V]:
+        if visited[i] == -1:
+            visited[i] = visited[V] + 1
+            DFS(Graph, i, visited)
 
 
 if __name__ == "__main__":
-    n=int(sys.stdin.readline())
-    target1,target2=map(int,sys.stdin.readline().split(' '))
+    n = int(sys.stdin.readline())
+    target1, target2 = map(int, sys.stdin.readline().split())
     m = int(sys.stdin.readline())
-    Graph=[]
-    for _ in range(n+1):
-        Graph.append([])
+
+    Graph = [[] for _ in range(n + 1)]
     for _ in range(m):
-        node1, node2 = map(int, sys.stdin.readline().split(' '))
+        node1, node2 = map(int, sys.stdin.readline().split())
         Graph[node1].append(node2)
         Graph[node2].append(node1)
 
-    print(BFS(Graph,target1)[target2])
-    #print(result)
+    visited = [-1] * (n + 1)
+    visited[target1]=0
+    DFS(Graph, target1, visited)
+    print(visited[target2])
