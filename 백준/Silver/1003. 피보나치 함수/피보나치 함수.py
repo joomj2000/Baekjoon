4cee34b5f348b3bb4ace3ca5952e1c9d8f104c9c
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-04-07 22:45:55 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,29 +1,9 @@
 import sys
-d=[0]*41
-# def fibonacci(n):
-# #     if n==0:
-# #         #print("0")
-# #         global zero
-# #         zero+=1
-# #         return 0
-# #     if n==1 :
-# #         global one
-# #         one += 1
-# #         return 1
-# #     if d[n]!=0:
-# #         return d[n]
-# #     d[n]=fibonacci(n-1)+fibonacci(n-2)
-# #     return d[n]
 
 d=[0]*41
 d[1]=1
 d[2]=1
-
-
 T = int(sys.stdin.readline())
-
-zero = 0
-one = 0
 for _ in range(T):
     N = int(sys.stdin.readline())
     for i in range(3,N+1):
@@ -33,6 +13,3 @@ for _ in range(T):
     else:
         print(d[N-1],end=' ')
     print(d[N])
-   # print(zero, one)
-    zero = 0
-    one = 0
