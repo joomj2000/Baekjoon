eba5e4eb1e3c05251c9a19f01ae5605f2476d322
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-04-29 20:50:00 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,52 +1,26 @@
 import sys
-from collections import deque
-def DFS(Graph, V, visited,result):
-    visited[V] =True
-    result+=1
-    #print(V,end=' ')
-    for i in Graph[V]:
-        if not visited[i]:
-            DFS(Graph,i,visited,result)
-    return result
 
-def BFS(Graph, start, visited):
-    queue=deque([start])
-    visited[start]=True
-    result=0
-    while queue:
-        result+=1
-        V=queue.popleft()
-        #print(V,end=' ')
-        for i in Graph[V]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i]=True
-    return result
+def DFS():
+    while(stack):
+        current=stack.pop()
+        global cnt
+        if current not in visited:
+            visited.append(current)
+            cnt+=1
+        for j in range(N+1):
+            if adj_matrix[current][j] and j not in visited:
+                stack.append(j)
 
-if __name__ == "__main__":
-    node=int(sys.stdin.readline())
-    line=int(sys.stdin.readline())
-    Graph=[]
-    for _ in range(node+1):
-        Graph.append([])
+N=int(sys.stdin.readline())
+M=int(sys.stdin.readline())
+cnt=0
+adj_matrix=[[0]*(N+1) for _ in range(N+1)]
+for _ in range(M):
+    start,end=map(int,sys.stdin.readline().split())
+    adj_matrix[start][end]=1
+    adj_matrix[end][start]=1
 
-    for _ in range(line):
-        node1, node2 = map(int, sys.stdin.readline().split(' '))
-        Graph[node1].append(node2)
-        Graph[node2].append(node1)
-
-    for i in range(node+1):
-        Graph[i].sort()
-
-    visited = [False] * (node + 1)
-    result=0
-    print(BFS(Graph,1,visited)-1)
-    #result=DFS(Graph,1,visited,result)
-    #print(result)
-    # print('')
-    # visited = [False] * (N + 1)
-    # BFS(Graph,V,visited)
-
-
-
-
+stack=[1]
+visited=[]
+DFS()
+print(cnt-1)
