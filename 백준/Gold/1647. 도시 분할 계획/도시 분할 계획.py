d0b6dca199151f2feb7f5f4514483f21fbabc1d1
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-05-07 12:03:29 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3464 ms, Memory: 271884 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,33 @@
+import sys
+def find_set(x): #연결된 최고 조상 확인
+    if p[x] != x:
+        p[x] = find_set(p[x]) # path compression
+    return p[x]
+
+def union(x, y): #차수가 더 작은 트리가 차수가 더 큰 트리 밑으로 들어감
+    if r[x] > r[y]:
+        p[y] = x
+    else:
+        p[x] = y
+        if r[x] == r[y]:
+            r[y] += 1
+
+
+V, E = map(int, sys.stdin.readline().split())
+edges = [list(map(int, sys.stdin.readline().split())) for _ in range(E)]
+edges.sort(key=lambda x: (x[2])) # 가중치 기준으로 정렬
+p = list(range(V+1))
+r = [0] * (V+1) #차수를 저장할 배열
+answer = []
+cnt = 0
+
+
+for s, e, w in edges:
+    if find_set(s) != find_set(e): #사이클이 아닐때
+        union(find_set(s), find_set(e)) #합치기
+        answer.append(w)
+        cnt += 1
+        if cnt == V:
+            break
+
+print(sum(answer)-max(answer))
