afef35afdb8f6adcca1fa49af876a7daebc439d4
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-06-04 20:35:04 +0900

    [Gold V] Title: 두 용액, Time: 124 ms, Memory: 44104 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,43 +1,27 @@
 import sys
+from itertools import combinations
+import bisect
 
-# 입력 받기
-input = sys.stdin.read
-data = input().split()
 
-# 용액의 개수
-N = int(data[0])
-
-# 용액 리스트
-water = list(map(int, data[1:]))
-
-# 정렬된 용액 리스트
+N=int(sys.stdin.readline())
+result=[]
+water=list(map(int,sys.stdin.readline().split()))
 water.sort()
-
-# 두 포인터 초기화
-left, right = 0, N - 1
-
-# 초기 값 설정
-answer = float('inf')
-result = [0, 0]
-
-# 두 포인터가 겹치지 않을 때까지 반복
-while left < right:
-    mix = water[left] + water[right]
-
-    # 현재 용액의 합이 0에 더 가까우면 갱신
-    if abs(mix) < abs(answer):
-        answer = mix
-        result = [water[left], water[right]]
-
-    # 합이 0이면 정답 출력 후 종료
-    if mix == 0:
-        break
-    # 합이 0보다 크면 오른쪽 포인터를 왼쪽으로 이동
-    elif mix > 0:
-        right -= 1
-    # 합이 0보다 작으면 왼쪽 포인터를 오른쪽으로 이동
+#print(water)
+start=0
+end=len(water)-1
+result=float('inf')
+result_water=[]
+while start<end:
+    target=water[start]+water[end]
+    if abs(result)>abs(target):
+        result=target
+        result_water=[water[start],water[end]]
+    if target==0:
+        print(*sorted([water[start],water[end]]))
+        exit()
+    elif target>0:
+        end-=1
     else:
-        left += 1
-
-# 결과 출력
-print(*sorted(result))
+        start+=1
+print(*sorted(result_water))
