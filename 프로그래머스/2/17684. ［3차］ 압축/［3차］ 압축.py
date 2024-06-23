d0278f800f9912a1573337e53a4fd04a7b67421b
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2024-06-24 00:06:51 +0900

    [level 2] Title: [3차] 압축, Time: 0.31 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,32 +1,11 @@
 def solution(msg):
     answer = []
-    dic = {}
-    for i in range(1, 27):
-        dic[chr(ord('A') + i - 1)] = i
-    # print(dic)
-    value = 27
-    while msg:
-        if len(msg)==1:
-            answer.append(dic[msg])
-            break
-        i = 0
-        j = 1
-        # if j>=len(msg):
-        #     break
-        while msg[i:j] in dic:
-            j += 1
-            if j>len(msg):
-                break
-        #print(msg)
-        #print(i, j)
-        # msg=msg[j-1:]
-        answer.append(dic[msg[i:j - 1]])
-        dic[msg[i:j]] = value
-        value += 1
-        #i = j - 1
-        #j = i + 1
-        msg = msg[j - 1:]
-        #print(dic)
-        # answer
-
+    dic={}
+    for i in range(1,27):
+        dic[ato(96+i)]=i
+    print(dic)
+        
+    
+    #for 
+    
     return answer
